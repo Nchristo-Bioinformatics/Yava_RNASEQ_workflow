ec37dc32985f673efa46917b44203694ae2ae2df
Nchristo-Bioinformatics <76002285+Nchristo-Bioinformatics@users.noreply.github.com>
2022-10-10 12:29:29 -0400

    Update yava_DE_analysis.R

--- yava_DE_analysis.R
+++ yava_DE_analysis.R
@@ -1,1 +1,9 @@
 #conda create -n sleuthing -c conda-forge -c bioconda r-essentials r-base
+#conda activate sleuthing
+#R
+chooseCRANmirror()
+install.packages("BiocManager")
+BiocManager::install("rhdf5")
+install.packages("devtools") 
+library("devtools")
+devtools::install_github("pachterlab/sleuth")
